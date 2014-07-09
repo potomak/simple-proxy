936f76fc17daea645aef7dc0267934471cb14399
Giovanni Cappellotto <potomak84@gmail.com>
2014-07-09 21:49:03 +0200

    Initial import

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple Proxy
+
+A very minimal HTTP proxy implemented in go.
+
+## Usage
+
+Start the server and visit [http://localhost:8080/i/?url=http://hackatron.org](http://localhost:8080/i/?url=http://hackatron.org).
---
+++ simple-proxy.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+    "fmt"
+    "net/http"
+    "io/ioutil"
+    "os"
+    )
+
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("Request: %s\n", r.URL)
+
+    url := r.URL.Query()["url"][0]
+
+    if url != "" {
+        response, err := http.Get(string(url))
+        if err != nil {
+            fmt.Print("%s", err)
+            os.Exit(1)
+        } else {
+            defer response.Body.Close()
+            contents, err := ioutil.ReadAll(response.Body)
+            if err != nil {
+              fmt.Print("%s", err)
+              os.Exit(1)
+            }
+            fmt.Fprintf(w, "%s", string(contents))
+        }
+    } else {
+      fmt.Fprintf(w, "URL param missing")
+    }
+}
+
+func main() {
+    http.HandleFunc("/i/", imageHandler)
+    http.ListenAndServe(":8080", nil)
+}
